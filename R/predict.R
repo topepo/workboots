d17183be447491f1228787df3611345e50e2dbb7
topepo <mxkuhn@gmail.com>
2024-02-01 08:05:30 -0500

    docs and some simplifications

--- R/predict.R
+++ R/predict.R
@@ -1,4 +1,11 @@
-#' Calculate bootstrap prediction intervals for new samples
+#' Calculate bootstrap prediction intervals for new samples.
+#'
+#' @param x An object of class `"bootstrapped_models"` created by
+#' [add_bootstrap_models()].
+#' @param new_data A data frame of predictor values.
+#' @param interval_width The coverage rate of the intervals (e.g. 0.95 for a
+#' 95% interval).
+#' @param ... Not used.
 #' @export
 #'
 predict.bootstrapped_models <- function(x, new_data, ..., interval_width) {
@@ -8,16 +15,18 @@ predict.bootstrapped_models <- function(x, new_data, ..., interval_width) {
       x$.models,
       predict_new,
       new_data = new_data,
-      future.packages = c("tidymodels", "earth"),
+      future.packages = required_pkgs(x$.models[[1]]$fit),
       future.seed = TRUE,
       future.stdout = TRUE
     )
+
   pred_res <- do.call("cbind", pred_res)
+  pred_res <- as.matrix(pred_res)
   low_quant <- (1 - interval_width) / 2
   high_quant <- 1 - low_quant
-  val_lower <- purrr::map_dbl(pred_res, ~ unname(quantile(.x, probs = low_quant, na.rm = TRUE)))
-  val_median <- purrr::map_dbl(pred_res, ~ median(.x, na.rm = TRUE))
-  val_upper <- purrr::map_dbl(pred_res, ~ unname(quantile(.x, probs = high_quant, na.rm = TRUE)))
+  val_lower <- apply(pred_res, 1, function(x) unname(quantile(x, probs = low_quant, na.rm = TRUE)))
+  val_median <- apply(pred_res, 1, function(x) median(x, na.rm = TRUE))
+  val_upper <- apply(pred_res, 1, function(x) unname(quantile(x, probs = high_quant, na.rm = TRUE)))
   tibble::tibble(.lower = val_lower, .pred = val_median, .upper = val_upper)
 }
 
